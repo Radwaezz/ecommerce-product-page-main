53a2c9abfab4d7af6928f442106b1b796fbd3f1b
Radwaezz <radwasalem1983@gmail.com>
2024-08-20 18:50:18 +0300

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import Navbar from './components/Navbar/Navbar';
 import Home from './Pages/Home';
 import Collections from './Pages/Collections';
 import Men from './Pages/Men';
-// import Women from './Pages/Women';
+import Women from './Pages/Women';
 import About from './Pages/About';
 import Contact from './Pages/Contact';
-import { CartProvider } from './components/CartContext/CartContext'; // Import CartProvider
+import { CartProvider } from './components/CartContext/CartContext'; 
 
 function App() {
   return (
@@ -18,7 +18,7 @@ function App() {
           <Route path="/women" element={<Home />} />
           <Route path="/collections" element={<Collections />} />
           <Route path="/men" element={<Men />} />
-          {/* <Route path="/women" element={<Women />} /> */}
+          <Route path="/women" element={<Women />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           {/* <Route path="/cart" element={<Cart />} /> */}
--- src/components/NewProductPage/NewProductPage.jsx
+++ src/components/NewProductPage/NewProductPage.jsx
@@ -1,33 +1,33 @@
-// NewProductPage.js
+NewProductPage.js
 // import React from 'react';
-// import ProductCard from '../ProductCard/ProductCard';
+import ProductCard from '../ProductCard/ProductCard';
 
-// const NewProductPage = () => {
-//   const newProduct = {
-//     id: 2,
-//     name: 'Spring Edition Sneakers',
-//     price: 130.00,
-//     originalPrice: 260.00,
-//     discount: 50,
-//     images: [
-//       '/images/men-main.webp',
-//       '/images/men-2.webp',
-//       '/images/men-3.webp',
-//       '/images/men-4.webp',
-//     ],
-//     thumbnails: [
-//       '/images/men-main.webp',
-//       '/images/men-2.webp',
-//       '/images/men-3.webp',
-//       '/images/men-4.webp',
-//     ],
-//   };
+const NewProductPage = () => {
+  const newProduct = {
+    id: 2,
+    name: 'Spring Edition Sneakers',
+    price: 130.00,
+    originalPrice: 260.00,
+    discount: 50,
+    images: [
+      '/images/men-main.webp',
+      '/images/men-2.webp',
+      '/images/men-3.webp',
+      '/images/men-4.webp',
+    ],
+    thumbnails: [
+      '/images/men-main.webp',
+      '/images/men-2.webp',
+      '/images/men-3.webp',
+      '/images/men-4.webp',
+    ],
+  };
 
-//   return (
-//     <div>
-//       <ProductCard product={newProduct} />
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      <ProductCard product={newProduct} />
+    </div>
+  );
+};
 
-// export default NewProductPage;
+export default NewProductPage;
